91cb571c5bbd168f6fa0b21509d6cd7c941810a7
m1kkY5 <50371463+m1kkY5@users.noreply.github.com>
2019-06-05 18:21:08 +0200

    Add files via upload

---
+++ Moje_Funkcije/Moje_Funkcije.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moje_Funkcije
+{
+    public class Nizovi_Matrice_N
+    {
+        private static Random rnd = new Random();
+
+        public void upisiNiz(out int[] niz)
+        {
+
+            Console.WriteLine("Koliko elemenata ima:");
+
+            int n = int.Parse(Console.ReadLine());
+            niz = new int[n];
+
+            for (int i = 0; i < n; i++)
+                niz[i] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+        }
+
+        public void randomNiz(out int[] niz)
+        {
+
+            Console.WriteLine("Koliko elemenata ima:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Opseg elemenata je: ");
+            string[] range = Console.ReadLine().Split(',');
+            Console.WriteLine();
+
+            int r1 = int.Parse(range[0]);
+            int r2 = int.Parse(range[1]);
+
+            niz = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int rd = rnd.Next(r1, r2);
+                niz[i] = rd;
+            }
+        }
+
+        public void ispisNiz(int[] niz)
+        {
+
+            int len = niz.Length;
+
+            Console.WriteLine("Elementi niza su: ");
+
+            for (int i = 0; i < len; i++)
+                Console.Write("{0} ", niz[i]);
+
+            Console.WriteLine();
+            
+        }
+
+        public void upisiMatricu(out int[,] matrica)
+        {
+            Console.WriteLine("Dimenzije matrice su: ");
+            string[] dim = Console.ReadLine().Split(',');
+
+            int x = int.Parse(dim[0]);
+            int y = int.Parse(dim[1]);
+
+            matrica = new int[x, y];
+
+            for (int i = 0; i < x; i++)
+            {
+                string[] elemnti = Console.ReadLine().Split(',');
+                for (int j = 0; j < y; j++)
+                {
+                    matrica[i, j] = int.Parse(elemnti[j]);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void randomMatrica(out int[,] matrica)
+        {
+
+            Console.WriteLine("Dimenzije matrice su: ");
+            string[] dim = Console.ReadLine().Split(',');
+            Console.WriteLine();
+
+            int x = int.Parse(dim[0]);
+            int y = int.Parse(dim[1]);
+
+            Console.WriteLine("Opseg elemenata je:");
+            string[] range = Console.ReadLine().Split(',');
+            Console.WriteLine();
+
+            int r1 = int.Parse(range[0]);
+            int r2 = int.Parse(range[1]);
+
+            matrica = new int[x, y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    int rd = rnd.Next(r1, r2);
+                    matrica[i, j] = rd;
+                }
+            }
+        }
+
+        public void ispisMatricu(int[,] matrica)
+        {
+
+            int x = matrica.GetLength(0);
+            int y = matrica.GetLength(1);
+
+            Console.Write("Matrica :");
+            Console.WriteLine();
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < y; j++)
+                    Console.Write("{0} ", matrica[i, j]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public void sort(ref int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+
+                int min_idx = i;
+
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        public void izdvoj(int[,] matrica)
+        {
+
+            int x = matrica.GetLength(0);
+            int y = matrica.GetLength(1);
+
+            Console.WriteLine("Opseg izdvajanja je: ");
+            string[] range = Console.ReadLine().Split(',');
+            int r1 = int.Parse(range[0]);
+            int r2 = int.Parse(range[1]);
+
+            int poz = 0;
+
+            int[] niz = new int[x * y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    int tmp = matrica[i, j];
+                    if (tmp >= r1 && tmp <= r2)
+                        niz[poz++] = tmp;
+                }
+            }
+            Console.WriteLine();
+
+            niz = niz.Where(T => T != 0).ToArray();
+            int len = niz.Length;
+            ispisNiz(niz);
+        }
+
+    }
+
+    public class Nizovi_Matrice_S { 
+      
+    }
+
+    public class Nizovi_Matrice_C {
+
+        private static Random rnd = new Random();
+
+        public void upisiNiz(out char[] niz)
+        {
+            Console.WriteLine("Koliko elemenata ima: ");
+
+            int n = int.Parse(Console.ReadLine());
+            niz = new char[n];
+            for (int i = 0; i < n; i++)
+                niz[i] = char.Parse(Console.ReadLine());
+        }
+
+        public void ispisNiz(char[] niz) {
+            int n = niz.Length;
+            Console.WriteLine("Elementi su: ");
+
+            for (int i = 0; i < n; i++)
+                Console.Write("{0} ",niz[i]);
+        }
+
+        public void ispisMatrica(char[,] matrica) {
+            int x = matrica.GetLength(0);
+            int y = matrica.GetLength(1);
+
+            for (int i = 0; i < x; i++) {
+                Console.WriteLine();
+                for (int j = 0; j < y; j++) {
+                    Console.Write("{0} ", matrica[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+    public class Brojevi 
+    {
+        public bool prost(int n) {
+            if (n == 2)
+                return false;
+            if (n % 2 == 0)
+                return false;
+            for(int i = 2; i < n; i++)
+                if (n%i==0)
+                    return false;
+            return true;
+        }
+
+        public int inverzan(int n) {
+            int novi = 0;
+            while (n > 0) {
+                int c = n % 10;
+                novi = novi * 10 + c;
+                n /= 10;
+            }
+            return novi;
+        }
+
+        public bool palindrom(int n) {
+            if (inverzan(n) == n)
+                return true;
+            else
+                return false;
+        }
+    }
+
+}
---
+++ Moje_Funkcije/igra.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Igra
+{
+    public class io
+    {
+        const int dim = 3;
+
+        private static void ispisMatrica(char[,] matrica)
+        {
+            int x = matrica.GetLength(0);
+            int y = matrica.GetLength(1);
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("{0} ", matrica[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void TIspis(char[,] tabela)
+        {
+            Console.Write(" {0} | {1} | {2}", tabela[0, 0], tabela[0, 1], tabela[0, 2]);
+            Console.WriteLine();
+            Console.WriteLine("------------");
+            Console.Write(" {0} | {1} | {2}", tabela[1, 0], tabela[1, 1], tabela[1, 2]);
+            Console.WriteLine();
+            Console.WriteLine("------------");
+            Console.Write(" {0} | {1} | {2}", tabela[2, 0], tabela[2, 1], tabela[2, 2]);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static void generisi(ref char[,] tabela)
+        {
+            string p = "abcdefghi";
+            int count = 0;
+
+            for (int i = 0; i < dim; i++)
+                for (int j = 0; j < dim; j++)
+                    tabela[i, j] = p[count++];
+        }
+
+        private static bool popunjeno(bool[,] a, int x, int y) {
+            if (a[x, y] == true)
+                return true;
+            else
+                return false;
+        }
+
+        private static void prvi(ref char[,] tabela, ref char[,] tabelaDisplay, ref bool[,]p)
+        {
+            Console.Write("I igrac\nPozicija: ");
+
+            //bool ponavljaj = true;
+
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.NumPad1:
+                    tabela[2, 0] = 'X';
+                    tabelaDisplay[2, 0] = 'X';
+                    p[2, 0] = true;
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad2:
+                    tabela[2, 1] = 'X';
+                    tabelaDisplay[2, 1] = 'X';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad3:
+                    tabela[2, 2] = 'X';
+                    tabelaDisplay[2, 2] = 'X';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad4:
+                    tabela[1, 0] = 'X';
+                    tabelaDisplay[1, 0] = 'X';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad5:
+                    tabela[1, 1] = 'X';
+                    tabelaDisplay[1, 1] = 'X';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad6:
+                    tabela[1, 2] = 'X';
+                    tabelaDisplay[1, 2] = 'X';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad7:
+                    tabela[0, 0] = 'X';
+                    tabelaDisplay[0, 0] = 'X';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad8:
+                    tabela[0, 1] = 'X';
+                    tabelaDisplay[0, 1] = 'X';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad9:
+                    tabela[0, 2] = 'X';
+                    tabelaDisplay[0, 2] = 'X';
+                    Console.Clear();
+                    break;
+            }
+        }
+
+        private static void drugi(ref char[,] tabela, ref char[,] tabelaDisplay, ref bool[,]p)
+        {
+            Console.Write("II igrac\nPozicija: ");
+
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.NumPad1:
+                        tabela[2, 0] = 'O';
+                        tabelaDisplay[2, 0] = 'O';
+                        Console.Clear();
+                        break;                    
+                case ConsoleKey.NumPad2:
+                    tabela[2, 1] = 'O';
+                    tabelaDisplay[2, 1] = 'O';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad3:
+                    tabela[2, 2] = 'O';
+                    tabelaDisplay[2, 2] = 'O';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad4:
+                    tabela[1, 0] = 'O';
+                    tabelaDisplay[1, 0] = 'O';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad5:
+                    tabela[1, 1] = 'O';
+                    tabelaDisplay[1, 1] = 'O';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad6:
+                    tabela[1, 2] = 'O';
+                    tabelaDisplay[1, 2] = 'O';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad7:
+                    tabela[0, 0] = 'O';
+                    tabelaDisplay[0, 0] = 'O';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad8:
+                    tabela[0, 1] = 'O';
+                    tabelaDisplay[0, 1] = 'O';
+                    Console.Clear();
+                    break;
+                case ConsoleKey.NumPad9:
+                    tabela[0, 2] = 'O';
+                    tabelaDisplay[0, 2] = 'O';
+                    Console.Clear();
+                    break;
+            }
+
+        }
+
+        private static bool proveriKolone(char[,] tabela)
+        {
+
+            if (tabela[0, 0] == tabela[1, 0] && tabela[1, 0] == tabela[2, 0] ||
+                tabela[0, 1] == tabela[1, 1] && tabela[1, 1] == tabela[2, 1] ||
+                tabela[0, 2] == tabela[1, 2] && tabela[1, 2] == tabela[2, 2])
+                return true;
+            else
+                return false;
+
+        }
+
+        private static bool proveriRedove(char[,] tabela)
+        {
+            if (tabela[0, 0] == tabela[0, 1] && tabela[0, 1] == tabela[0, 2] ||
+                tabela[1, 0] == tabela[1, 1] && tabela[1, 1] == tabela[1, 2] ||
+                tabela[2, 0] == tabela[2, 1] && tabela[2, 1] == tabela[2, 2])
+                return true;
+            else
+                return false;
+        }
+
+        private static bool proveriDijagonale(char[,] tabela)
+        {
+            if (tabela[0, 0] == tabela[1, 1] && tabela[1, 1] == tabela[2, 2] ||
+                tabela[2, 0] == tabela[1, 1] && tabela[1, 1] == tabela[0, 2])
+                return true;
+            else
+                return false;
+        }
+
+        public static void play()
+        {
+            char[,] tabelaDisplay = new char[dim, dim];
+            char[,] tabela = new char[dim, dim];
+            bool[,] popunjeno = new bool[dim, dim];
+
+            generisi(ref tabela);
+
+            int potez = 1;
+            
+            TIspis(tabelaDisplay);
+                while (potez<10) {
+
+                    if (potez % 2 == 1)
+                        prvi(ref tabela, ref tabelaDisplay, ref popunjeno);
+                    else
+                        drugi(ref tabela, ref tabelaDisplay, ref popunjeno);
+
+                    if (proveriKolone(tabela) == true ||
+                        proveriRedove(tabela) == true ||
+                        proveriDijagonale(tabela) == true)
+                        break;
+
+                    potez++;
+                    TIspis(tabelaDisplay);
+                    
+                    //Console.WriteLine();
+          
+            }
+                Thread.Sleep(1000);
+                Console.Clear();
+
+                int c = potez - 1;
+                
+            if (c % 2 == 1)
+                    Console.WriteLine("Igrac II je pobedio nakon {0} poteza", c);
+                else
+                    Console.WriteLine("Igrac I je pobesio nakon {0} poteza", c);
+
+                
+        }
+
+    }
+}
